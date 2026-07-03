0ac2a95012f62b05b026d2848dfeb7f0da62f906
GabrielP <gapc1418@gmail.com>
2026-07-02 21:14:59 -0500

    fff

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Prefijo global opcional
-  app.setGlobalPrefix('api');
+
+  // --- CONFIGURACIÓN DE SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('API de Tickora')
+    .setDescription('Documentación interactiva de los endpoints de Tickora')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  
+  // Aquí definimos que Swagger vivirá en la ruta /api/docs
+  SwaggerModule.setup('api/docs', app, document);
+
+  // Habilitar CORS (super útil para cuando conectes tu frontend)
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
